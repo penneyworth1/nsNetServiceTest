3e010a7880d11bff2f6572362aaf43e50700c56e
Steven Stewart <stevenstewart@Stevens-MacBook-Pro-2.local>
2014-11-11 17:49:00 -0600

    asdf

---
+++ NetServiceTest/DataHandler.h
@@ -0,0 +1,13 @@
+//
+//  DataHandler.h
+//  NetServiceTest
+//
+//  Created by Steven Stewart on 11/11/14.
+//  Copyright (c) 2014 Steven Stewart. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DataHandler : NSObject
+
+@end
---
+++ NetServiceTest/DataHandler.m
@@ -0,0 +1,13 @@
+//
+//  DataHandler.m
+//  NetServiceTest
+//
+//  Created by Steven Stewart on 11/11/14.
+//  Copyright (c) 2014 Steven Stewart. All rights reserved.
+//
+
+#import "DataHandler.h"
+
+@implementation DataHandler
+
+@end
